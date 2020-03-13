cb50f5877e1957b2666dc334fb7e8798d65f909a
manu.delay@wanadoo.fr <manu.delay@wanadoo.fr>
2020-03-13 22:30:15 +0100

    Ajout de l'option --nograph

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 # Gal
 Générateur d'analyse lexicale
 
-Utilisation : ```bin/main exp``` où exp est un fichier contenant une
-expression rationnelle simple finissant par \\n.
+Utilisation : ```bin/main [--nograph] exp``` où `exp` est un fichier
+contenant une expression rationnelle simple finissant par '\\n'.
 
-Génère des fichiers au format dot dans le réperoire `dot_src` et les pdf
-correspondants dans le répertoire `pdf`.
+L'option `--nograph` permet de ne pas générer la représentation graphique
+avec `dot`. Sinon, génère des fichiers au format dot dans le réperoire
+ `dot_src` et les pdf correspondants dans le répertoire `pdf`.
 
 Le code source de l'analyseur lexical lui-même est construit dans le
 réperoire `analyseur_src`.
--- src/main.c
+++ src/main.c
@@ -8,13 +8,35 @@
 #include "dfa_min.h"
 #include "gal.h"
 
+/*
+ * Affiche la façon d'utiliser le programme.
+ */
 void usage(char *nom) {
-    fprintf(stderr, "Usage : %s exp\n", nom);
+    fprintf(stderr, "Usage : %s [--nograph] exp\n", nom);
     fprintf(stderr, "    exp est un fichier contenant une expression"
             " rationnelle sur une ligne, finissant par \\n.\n");
+    fprintf(stderr, "    l'option --nograph permet de ne pas générer"
+            " les représentations graphiques.\n");
 }
 
+/*
+ * Génère le pdf grâce à dot.
+ */
+void dot(char *filename, char *extname, char *ext) {
+    char *dotstring = malloc(sizeof(char) * (2*strlen(extname)+50));
+    sprintf(dotstring,
+            "dot -T pdf -o pdf/%s_%s.pdf %s", filename, ext, extname);
+    system(dotstring);
+}
+
+
 int main(int argc, char **args) {
+    unsigned char gen_graph = 1;
+    if (argc >= 2 && strcmp(args[1], "--nograph")==0) {
+        gen_graph = 0;
+        args[1] = args[2];
+        argc--;
+    }
     if (argc != 2) {
         usage(args[0]);
         exit(1);
@@ -31,35 +53,38 @@ int main(int argc, char **args) {
     TREE tree = npi_to_tree(postfix);
     char *extname = malloc(sizeof(char) * (strlen(filename)+50));
     sprintf(extname, "dot_src/%s.tree", filename);
-    printf("Création de %s_tree.pdf ...\n", filename);
-    tree2file(tree, extname, exp);
-    char *dotstring = malloc(sizeof(char) * (2*strlen(extname)+50));
-    sprintf(dotstring, "dot -T pdf -o pdf/%s_tree.pdf %s", filename, extname);
-    system(dotstring);
+    if (gen_graph) {
+        printf("Création de %s_tree.pdf ...\n", filename);
+        tree2file(tree, extname, exp);
+        dot(filename, extname, "tree");
+    }
 
     printf("Conversion en NFA ...\n");
     NFA nfa = tree2nfa(tree);
     sprintf(extname, "dot_src/%s.nfa", filename);
-    printf("Création de %s_nfa.pdf ...\n", filename);
-    nfa2file(nfa, extname, exp);
-    sprintf(dotstring, "dot -T pdf -o pdf/%s_nfa.pdf %s", filename, extname);
-    system(dotstring);
+    nfa2file(nfa, extname, exp); // obligatoire pour numéroter les états
+    if (gen_graph) {
+        printf("Création de %s_nfa.pdf ...\n", filename);
+        dot(filename, extname, "nfa");
+    }
 
     printf("Conversion en DFA ...\n");
     DFA dfa = nfa2dfa(nfa);
     sprintf(extname, "dot_src/%s.dfa", filename);
-    printf("Création de %s_dfa.pdf ...\n", filename);
-    dfa2file(dfa, extname, exp);
-    sprintf(dotstring, "dot -T pdf -o pdf/%s_dfa.pdf %s", filename, extname);
-    system(dotstring);
+    if (gen_graph) {
+        printf("Création de %s_dfa.pdf ...\n", filename);
+        dfa2file(dfa, extname, exp);
+        dot(filename, extname, "dfa");
+    }
 
     printf("Minimisation du DFA ...\n");
     DFAMIN dfamin = dfa2min(dfa);
     sprintf(extname, "dot_src/%s.min", filename);
-    printf("Création de %s_min.pdf ...\n", filename);
-    dfamin2file(dfamin, extname, exp);
-    sprintf(dotstring, "dot -T pdf -o pdf/%s_min.pdf %s", filename, extname);
-    system(dotstring);
+    if (gen_graph) {
+        printf("Création de %s_min.pdf ...\n", filename);
+        dfamin2file(dfamin, extname, exp);
+        dot(filename, extname, "min");
+    }
 
     printf("Génération de l'analyseur lexical ...\n");
     sprintf(extname, "analyseur_src/%s.c", filename);
